55feef5ab3ce0817f58e7bb01fc7a74bb61c5472
AndreaMiranda <martinezmirandandre2@gmail.com>
2026-08-15 01:10:46 -0400

    Agrega service worker para modo app y uso offline básico

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'seber-regresion-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => {
+        if (event.request.url.startsWith(self.location.origin)) cache.put(event.request, copy);
+      });
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
